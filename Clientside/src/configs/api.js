5b207a71fd7a9bc88d9ee69d57e9495c1955c407
vineetweb-ui <vineegadwali@gmail.com>
2026-03-22 20:11:31 +0530

    Update api.js

--- Clientside/src/configs/api.js
+++ Clientside/src/configs/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://ai-resume-builder-jqo6.onrender.com'
+  baseURL: 'https://ai-resume-builder-v2q4.onrender.com'
 });
 
 export default api;
